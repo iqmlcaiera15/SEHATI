7f0f0217a6a3a8beca37981236094e1f817ad1a8
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-03-10 12:06:04 +0700

    update migration

--- database/migrations/2025_02_03_185512_bonding_activities.php
+++ database/migrations/2025_02_03_185512_bonding_activities.php
@@ -11,7 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('bonding', function (Blueprint $table) {
+            $table->id('bonding_id'); 
+            $table->string('bonding_header');
+            $table->string('activity');
+            $table->string('gambar');
+        });
     }
 
     /**
