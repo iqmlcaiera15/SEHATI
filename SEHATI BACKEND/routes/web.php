22021f4ba80d45783157eb66d2a1945d2dc72886
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-04-24 14:10:58 +0700

    Update web.php

--- SEHATI BACKEND/routes/web.php
+++ SEHATI BACKEND/routes/web.php
@@ -7,6 +7,12 @@ use App\Http\Controllers\CatatanController;
 use App\Http\Controllers\PostpartumController;
 use App\Http\Controllers\KomunitasController;
 
+use App\Http\Controllers\PrediksiDepresiController;
+use App\Http\Controllers\RekomendasiMakananController;
+use App\Http\Controllers\KickCounterController;
+use App\Http\Controllers\SkorEpdsController;
+
+
 
 
 #Air Quality
@@ -39,8 +45,27 @@ Route::get('/Recovery/history', [DeteksiController::class, 'histindex']);
 #Saran Makanan
 Route::get('/Recovery', [DeteksiController::class, 'index']);
 Route::get('/Recovery/history', [DeteksiController::class, 'histindex']);
-
 Route::get('/home', [HomeProfileController::class, 'home']);
+
+#Prediksi Depresi
+Route::get('/prediksidepresi', [PrediksiDepresiController::class, 'index']);
+Route::post('/prediksidepresi', [PrediksiDepresiController::class, 'store']);
+Route::get('/prediksidepresi/{id}', [PrediksiDepresiController::class, 'show']);
+Route::delete('/prediksidepresi/delete/{id}', [PrediksiDepresiController::class, 'deletebyID']);
+
+
+#EPDS
+Route::post('/epds', [SkorEpdsController::class, 'store']);
+Route::get('/epds', [SkorEpdsController::class, 'index']);
+Route::get('/epds/{id}', [SkorEpdsController::class, 'show']);
+
+#Rekomendasi Makanan
+Route::get('/rekomendasimakanan', [RekomendasiMakananController::class, 'index']);
+Route::get('/rekomendasimakanan/{id}', [RekomendasiMakananController::class, 'show']);
+
+#Kick Counter
+Route::get('/kick-counter', [KickCounterController::class, 'index']);
+Route::post('/kick-counter', [KickCounterController::class, 'store']);
 
 
 
