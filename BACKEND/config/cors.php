d2ea046a3a01bdefa3f85acbe50a26e07e0beea7
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-04-27 15:47:25 +0700

    Update cors.php

--- BACKEND/config/cors.php
+++ BACKEND/config/cors.php
@@ -4,11 +4,11 @@ return [
 
     'paths' => ['api/*', '*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'],  // Bisa diganti dengan domain frontend tertentu
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false,
+    'max_age' => 3600,  // Setel preflight request cache selama 1 jam
+    'supports_credentials' => false, // Ganti ke true jika diperlukan untuk cookie/authorization header
 
 ];
