4fe645454be6b31e001e0f885fa9329d3ea03a9a
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-04-28 21:13:16 +0700

    Update Komunitas.php

--- BACKEND/app/Models/Komunitas.php
+++ BACKEND/app/Models/Komunitas.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class DeteksiPenyakit extends Model
+class Komunitas extends Model
 {
     use HasFactory;
 
@@ -16,7 +16,6 @@ class DeteksiPenyakit extends Model
         'user_id', 
         'judul', 
         'deskripsi', 
-        'tekanan_darah', 
         'komen', 
         'likes'
     ];
