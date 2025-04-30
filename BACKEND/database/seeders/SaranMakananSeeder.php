7e700d1d3deb9082f604a635941a53bfc9930b56
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-04-30 21:58:52 +0700

    Update SaranMakananSeeder.php

--- BACKEND/database/seeders/SaranMakananSeeder.php
+++ BACKEND/database/seeders/SaranMakananSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,72 +13,77 @@ class SaranMakananSeeder extends Seeder
     public function run(): void
     {
         $Makanans = [
-            
-                [
-                    'kategori' => 'Buah',
-                    'nama_makanan' => 'Pisang',
-                    'deskripsi' => 'Kaya akan kalium dan serat, baik untuk pencernaan.',
-                    'cocok' => 'ibu hamil',
-                ],
-                [
-                    'kategori' => 'Sayuran',
-                    'nama_makanan' => 'Bayam',
-                    'deskripsi' => 'Mengandung zat besi dan folat, penting untuk perkembangan janin.',
-                    'cocok' => 'ibu hamil',
-                ],
-                [
-                    'kategori' => 'Protein',
-                    'nama_makanan' => 'Telur Rebus',
-                    'deskripsi' => 'Sumber protein dan kolin yang baik untuk otak janin.',
-                    'cocok' => 'ibu hamil',
-                ],
-                [
-                    'kategori' => 'Buah',
-                    'nama_makanan' => 'Alpukat',
-                    'deskripsi' => 'Kaya lemak sehat dan folat, baik untuk perkembangan otak bayi.',
-                    'cocok' => 'ibu menyusui',
-                ],
-                [
-                    'kategori' => 'Sayuran',
-                    'nama_makanan' => 'Brokoli',
-                    'deskripsi' => 'Mengandung kalsium dan vitamin C, baik untuk produksi ASI.',
-                    'cocok' => 'ibu menyusui',
-                ],
-                [
-                    'kategori' => 'Protein',
-                    'nama_makanan' => 'Ikan Salmon',
-                    'deskripsi' => 'Kaya omega-3, baik untuk perkembangan otak bayi.',
-                    'cocok' => 'ibu menyusui',
-                ],
-                [
-                    'kategori' => 'Buah',
-                    'nama_makanan' => 'Apel',
-                    'deskripsi' => 'Mudah dicerna dan kaya serat, cocok untuk bayi.',
-                    'cocok' => 'bayi',
-                ],
-                [
-                    'kategori' => 'Sayuran',
-                    'nama_makanan' => 'Wortel',
-                    'deskripsi' => 'Kaya vitamin A, baik untuk penglihatan bayi.',
-                    'cocok' => 'bayi',
-                ],
-                [
-                    'kategori' => 'Protein',
-                    'nama_makanan' => 'Daging Ayam',
-                    'deskripsi' => 'Sumber protein yang baik untuk pertumbuhan bayi.',
-                    'cocok' => 'bayi',
-                ],
-                [
-                    'kategori' => 'Buah',
-                    'nama_makanan' => 'Pir',
-                    'deskripsi' => 'Lembut dan mudah dicerna, cocok untuk bayi.',
-                    'cocok' => 'bayi',
-                ],
+            [
+                'nama' => 'Pisang',
+                'deskripsi' => 'Kaya akan kalium dan serat, baik untuk pencernaan.',
+                'target_makanan' => ['Hamil'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Bayam',
+                'deskripsi' => 'Mengandung zat besi dan folat, penting untuk perkembangan janin.',
+                'target_makanan' => ['Hamil'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Telur Rebus',
+                'deskripsi' => 'Sumber protein dan kolin yang baik untuk otak janin.',
+                'target_makanan' => ['Hamil'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Alpukat',
+                'deskripsi' => 'Kaya lemak sehat dan folat, baik untuk perkembangan otak bayi.',
+                'target_makanan' => ['Menyusui'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Brokoli',
+                'deskripsi' => 'Mengandung kalsium dan vitamin C, baik untuk produksi ASI.',
+                'target_makanan' => ['Menyusui'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Ikan Salmon',
+                'deskripsi' => 'Kaya omega-3, baik untuk perkembangan otak bayi.',
+                'target_makanan' => ['Menyusui'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Apel',
+                'deskripsi' => 'Mudah dicerna dan kaya serat, cocok untuk bayi.',
+                'target_makanan' => ['Bayi'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Wortel',
+                'deskripsi' => 'Kaya vitamin A, baik untuk penglihatan bayi.',
+                'target_makanan' => ['Bayi'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Daging Ayam',
+                'deskripsi' => 'Sumber protein yang baik untuk pertumbuhan bayi.',
+                'target_makanan' => ['Bayi'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
+            [
+                'nama' => 'Pir',
+                'deskripsi' => 'Lembut dan mudah dicerna, cocok untuk bayi.',
+                'target_makanan' => ['Bayi'],
+                'gambar' => 'https://amazon-datazone-sehatiapp.s3.ap-southeast-1.amazonaws.com/cara-mengawinkan-ayam-bangkok.webp',
+            ],
         ];
 
-        foreach ($Makanans as $Makanan)
-        {
-            DB::table('saran_makanan')->insert($Makanan);
+        foreach ($Makanans as $makanan) {
+            DB::table('rekomendasi_makanan')->insert([
+                'nama' => $makanan['nama'],
+                'deskripsi' => $makanan['deskripsi'],
+                'gambar' => $makanan['gambar'],
+                'target_makanan' => json_encode($makanan['target_makanan']),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
         }
     }
 }
