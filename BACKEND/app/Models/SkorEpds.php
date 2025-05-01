57612a59f1a8edd7d3242c54dd651465cd7538e0
erinazahira <91593559+erinazahira@users.noreply.github.com>
2025-05-01 18:06:51 +0700

    Update SkorEpds.php

--- BACKEND/app/Models/SkorEpds.php
+++ BACKEND/app/Models/SkorEpds.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use 
 
 class SkorEpds extends Model
 {
@@ -13,10 +14,17 @@ class SkorEpds extends Model
 
     protected $fillable = [
         'answers',
-        'score'
+        'score',
+        'hasil_epds',
+        'prediksi_depresi_id'
     ];
 
     protected $casts = [
         'answers' => 'array',
     ];
+
+    public function prediksi()
+    {
+        return $this->belongsTo(PrediksiDepresi::class, 'prediksi_depresi_id');
+    }
 }
