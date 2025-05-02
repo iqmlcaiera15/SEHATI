7b9603b374dd52f92b1e0e32c6873d16c4e611c4
erinazahira <erinazahira26@gmail.com>
2025-05-02 19:21:16 +0700

    komunitas

--- BACKEND/database/migrations/2025_05_02_121917_create_komenkomunitas_table.php
+++ BACKEND/database/migrations/2025_05_02_121917_create_komenkomunitas_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKomentarKomunitasTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('komenkomunitas', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
-            // $table->string('user_id'); 
-            $table->string('komentar');
             $table->timestamps();
-
-            $table->foreign('post_id')->references('post_id')->on('komunitas')->onDelete('cascade');
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('komenkomunitas');
     }
-}
-
+};
