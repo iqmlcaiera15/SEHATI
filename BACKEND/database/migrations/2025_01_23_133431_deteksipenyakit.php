db1775125be944e09487292769a016285fc990c5
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-05-08 23:46:06 +0700

    Update 2025_01_23_133431_deteksipenyakit.php

--- BACKEND/database/migrations/2025_01_23_133431_deteksipenyakit.php
+++ BACKEND/database/migrations/2025_01_23_133431_deteksipenyakit.php
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('deteksipenyakit', function (Blueprint $table) {
             $table->id('deteksi_id'); 
-            $table->foreignId('user_id')
-            ->constrained('users')
-            ->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('bidan_id')->nullable(); 
             $table->string('nama')->nullable(); 
             $table->integer('pregnancies')->nullable(); 
