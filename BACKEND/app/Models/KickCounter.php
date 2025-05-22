c8193cad0d030923c5796f2eb94985fe5f2ef19e
erinazahira <91593559+erinazahira@users.noreply.github.com>
2025-05-22 19:03:06 +0700

    Update KickCounter.php

--- BACKEND/app/Models/KickCounter.php
+++ BACKEND/app/Models/KickCounter.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,10 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class KickCounter extends Model
 {
     use HasFactory;
-
+    
     protected $fillable = [
+        'user_id', 
         'kick_count', 
         'recorded_at',
         'duration',
     ];
+    
+    protected $casts = [
+        'recorded_at' => 'datetime',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
