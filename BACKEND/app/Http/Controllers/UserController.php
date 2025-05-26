0b13d6cc768dcf26b77d90b643bc5bcf131788ee
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-05-26 22:19:42 +0700

    Update UserController.php

--- BACKEND/app/Http/Controllers/UserController.php
+++ BACKEND/app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
 use App\Models\Icon; 
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
