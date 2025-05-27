37fc1a8f90c9261ea1ccb2ea2f5a22e369d8b757
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-05-27 11:15:30 +0700

    Update 0001_01_01_000000_create_users_table.php

--- BACKEND/database/migrations/0001_01_01_000000_create_users_table.php
+++ BACKEND/database/migrations/0001_01_01_000000_create_users_table.php
@@ -33,7 +33,7 @@ return new class extends Migration
             $table->integer('usia_suami')->nullable();
             $table->string('pekerjaan_suami')->nullable();
             $table->integer('saldo_total')->nullable()->default(0);
-            $table->string('profil')->nullable();
+            $table->integer('selected_icon_id')->nullable();
             // $table->integer('jumlah_kehamilan_sebelumnya')->nullable();
             // $table->integer('jumlah_anak_hidup')->nullable();
             // $table->date('hpht')->nullable();
