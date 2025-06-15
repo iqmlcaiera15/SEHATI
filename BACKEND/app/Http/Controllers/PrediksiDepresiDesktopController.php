daa9edbf18eea17885308eb7d3462e755366546e
erinazahira <erinazahira26@gmail.com>
2025-06-15 19:16:45 +0700

    Update PrediksiDepresiDesktopController.php

--- BACKEND/app/Http/Controllers/PrediksiDepresiDesktopController.php
+++ BACKEND/app/Http/Controllers/PrediksiDepresiDesktopController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\PrediksiDepresi;
 
-class DepresiDesktopontroller extends Controller
+class DepresiDesktopController extends Controller
 {
     public function index(Request $request)
     {
