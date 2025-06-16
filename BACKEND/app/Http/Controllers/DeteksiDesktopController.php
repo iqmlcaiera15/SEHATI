7144070bb4407b15193a1d6bcb81099316746fcc
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-06-16 17:59:28 +0700

    Update DeteksiDesktopController.php

--- BACKEND/app/Http/Controllers/DeteksiDesktopController.php
+++ BACKEND/app/Http/Controllers/DeteksiDesktopController.php
@@ -218,7 +218,7 @@ class DeteksiDesktopController extends Controller
                         'predictions' => $prediction
                     ]);
 
-                    return redirect()->route('deteksi.show', $deteksi->id)
+                    return redirect()->route('deteksi.show', $deteksi->id ?? 1)
                         ->with('success', 'Data berhasil disimpan dan prediksi telah diterima.');
                 } else {
                     Log::error('Unexpected response format from ML API', [
